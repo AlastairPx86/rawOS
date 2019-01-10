c8cf3f64b5fd635fd1e317d268d7ed5d95bf3cf8
AlastairP <jacob_paul@outlook.com>
2019-01-10 16:42:35 +0100

    s

--- src/rawOS/io.asm
+++ src/rawOS/io.asm
@@ -60,6 +60,9 @@ ros_io_tostring_repeat:
     cmp di, string ; Check if we're at the start
     je ros_io_tostring_done ; If so: we're done
 
+    cmp ax, 0 ; Check if we've done everything
+    je ros_io_tostring_done
+
     sub di, 1 ; Move the reader upwards to the next byte
     mov dx, 0 ; Clear dx
     
@@ -68,5 +71,5 @@ ros_io_tostring_repeat:
 ros_io_tostring_done:
     mov [stringLoc], di
     popa
-    mov si, stringLoc
+    mov si, [stringLoc]
     ret
